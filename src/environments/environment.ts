962bc057efb7f421735fc427627c1f5c09671f8f
shivang bhargave <shivangbhargave99@gmail.com>
2021-07-21 07:27:57 -0700

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCqZoYe8vkJz-67JuZXSvaSJtpnfsWRZgI",
+    authDomain: "basic-banking-system-94128.firebaseapp.com",
+    projectId: "basic-banking-system-94128",
+    storageBucket: "basic-banking-system-94128.appspot.com",
+    messagingSenderId: "499095488300",
+    appId: "1:499095488300:web:d864f05dd27330c66e0e89",
+    measurementId: "G-XRTS61YM79"
+  }
+};
